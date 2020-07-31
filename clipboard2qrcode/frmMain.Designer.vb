1895af3ecc642b4efeee4a72966d9de545e0546b
t-midorikawa <t-midorikawa@kyouikusangyou.co.jp>
2020-07-31 12:00:44 +0900

    プロジェクト ファイルを追加します。

---
+++ clipboard2qrcode/frmMain.Designer.vb
@@ -0,0 +1,54 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.picQR = New System.Windows.Forms.PictureBox()
+        CType(Me.picQR, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'picQR
+        '
+        Me.picQR.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.picQR.Location = New System.Drawing.Point(0, 0)
+        Me.picQR.Name = "picQR"
+        Me.picQR.Size = New System.Drawing.Size(200, 200)
+        Me.picQR.TabIndex = 0
+        Me.picQR.TabStop = False
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(200, 200)
+        Me.Controls.Add(Me.picQR)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmMain"
+        Me.Text = "clipboard2qrcode"
+        CType(Me.picQR, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents picQR As PictureBox
+End Class
---
+++ clipboard2qrcode/frmMain.vb
@@ -0,0 +1,22 @@
+﻿Public Class frmMain
+
+    Private Sub picQR_Paint(sender As Object, e As PaintEventArgs) Handles picQR.Paint
+        If (Clipboard.ContainsText() = True) Then
+            Dim strText As String = Clipboard.GetText()
+            Dim bc1 As DotNetBarcode = New DotNetBarcode
+            bc1.Type = DotNetBarcode.Types.QRCode
+            bc1.PrintCheckDigitChar = True
+            bc1.WriteBar(strText, 0, 0, picQR.Width, picQR.Height, e.Graphics)
+        Else
+            picQR.Image = Nothing
+        End If
+    End Sub
+
+    Private Sub picQR_Resize(sender As Object, e As EventArgs) Handles picQR.Resize
+        picQR.Refresh()
+    End Sub
+
+    Private Sub picQR_MouseClick(sender As Object, e As MouseEventArgs) Handles picQR.MouseClick
+        picQR.Refresh()
+    End Sub
+End Class
